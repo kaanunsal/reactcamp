15b06a0779607314c7e4866a55cc6dbe14a24544
Kaan Ünsal <kunsalll@outlook.com>
2021-06-23 23:25:24 +0300

    23 Haziran 2325

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,3 +1,4 @@
+  
 import React from 'react'
 import { Dropdown, Menu,Image } from 'semantic-ui-react'
 
@@ -5,7 +6,7 @@ export default function SignedIn({signOut}) {
     return (
         <div>
             <Menu.Item>
-                <Image avatar spaced="right" src="https://yt3.ggpht.com/ytc/AAUvwngwJNZN3WObqCSP3MlLYGiR3cGFXbOkO3vWScSp2A=s900-c-k-c0x00ffffff-no-rj"/>
+                <Image avatar spaced="right" src="https://pbs.twimg.com/profile_images/1404479261566713856/_MklDkhx_400x400.jpg"/>
                 <Dropdown pointing="top left" text="Engin">
                     <Dropdown.Menu>
                         <Dropdown.Item text="Bilgilerim" icon="info"/>
